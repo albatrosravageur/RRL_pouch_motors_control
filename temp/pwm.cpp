10b02e9decdc8b04ad662379e275384dae4c67b0
albatrosravageur <yves.mrtn98@gmail.com>
2021-04-14 18:36:56 +0200

    Re

--- temp/pwm.cpp
+++ temp/pwm.cpp
@@ -15,12 +15,14 @@
 #define GND_PIN3 9
 #define GND_PIN4 12
 
-#define nb_samples 10 // the highest the most precise, but also the slowest
+#define nb_samples 1 // the highest the most precise, but also the slowest
 
 int pwm_value1;
 int pwm_value2;
 int pwm_value3;
 int pwm_value4;
+
+#define demo_mode // To comment when quitting debugging
 
 #ifdef demo_mode
 unsigned long time;
@@ -49,14 +51,14 @@ void setup()
 	pinMode(GND_PIN2, OUTPUT);
 	pinMode(GND_PIN3, OUTPUT);
 	pinMode(GND_PIN4, OUTPUT);
-	pinMode(GND_PIN1, LOW);
-	pinMode(GND_PIN2, LOW);
-	pinMode(GND_PIN3, LOW);
-	pinMode(GND_PIN4, LOW);
+	digitalWrite(GND_PIN1, LOW);
+	digitalWrite(GND_PIN2, LOW);
+	digitalWrite(GND_PIN3, LOW);
+	digitalWrite(GND_PIN4, LOW);
 
 	Serial.begin(9600);
 
-#ifdef demo_mode
+/*#ifdef demo_mode
 	time = micros();
 	for (int i = 0; i < 1000; i++)
 	{
@@ -73,7 +75,7 @@ void setup()
 		delay(1000);
 		Serial.println(".");
 	}
-#endif
+#endif*/
 }
 
 void loop()
@@ -118,5 +120,5 @@ void loop()
 	Serial.print(pwm_value4);
 	Serial.flush();
 #endif
-	delay(200);
+	delay(5);
 }
